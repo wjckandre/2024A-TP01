cc7791620266553a862eaaa1ae3e933dcee46ccb
wjckandre <wojcik.andre2.0@gmail.com>
2024-09-11 19:15:51 -0400

    ex 1

--- ex1.py
+++ ex1.py
@@ -1,5 +1,13 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est le nom de l'athlète ? ")
+date = input("Quel est la date du record ? ")
+discipline = input("En quelle discipline ? ")
+categorie = input("En quelle catégorie ? ")
+record = input("Quel est le record ? ")
+print("NOUVEAU RECORD")
+print("-----------------------------")
+print(date)
+print(discipline, " - ", categorie)
+print(athlete, country, " - ", record)
--- ex2.py
+++ ex2.py
@@ -1,4 +1,8 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
 
-water_quantity = ...
+water_quantity = int(input("Quel est la quantité d'eau a assainir ? "))
+print("Voici les matériaux requis pour l'assainissement de ", water_quantity, "L d'eau :")
+print(f"- {water_quantity/5} filtre(s)")
+print(f"- {water_quantity*3/5} lampes UV")
+print(f"- {water_quantity*0.5/5} kg de chlore")
